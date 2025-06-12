d919f572c591a12b316b2ca66034ecd86e073387
Ivan Fraixedes <ivan@fraixed.es>
2025-06-12 20:04:54 +0200

    01/solutions/05: Add 2 different solutions 2 matches (#1)
    Add 2 different solutions for the match parentheses exercise using 2
    match statements
    
    Both of them use `std::vec::Vec` as a stack, but one iterate the string
    with a `for` loop and with an iterator.

---
+++ 01/solutions/05-match-parentheses/src/for_loop_2_matches.rs
@@ -0,0 +1,97 @@
+fn match_parentheses(s: &str) -> bool {
+    use std::vec::Vec;
+    let mut stack: Vec<char> = Vec::with_capacity(if s.len() <= 100 { s.len() } else { 100 });
+
+    for c in s.chars() {
+        match c {
+            '(' | '[' | '{' => stack.push(c),
+            ')' | ']' | '}' => {
+                let correct = match stack.pop() {
+                    Some('(') => c == ')',
+                    Some('[') => c == ']',
+                    Some('{') => c == '}',
+                    Some(_) => {
+                        panic!("BUG: stack cannot contain any other character than '(', '[', '{{'")
+                    }
+                    None => false,
+                };
+
+                if !correct {
+                    return false;
+                }
+            }
+            _ => {}
+        }
+    }
+
+    stack.is_empty()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::match_parentheses;
+
+    #[test]
+    fn match_parentheses_empty() {
+        assert!(match_parentheses(""));
+        assert!(match_parentheses("foobar"));
+    }
+
+    #[test]
+    fn match_parentheses_wrong_order() {
+        assert!(!match_parentheses(")("));
+        assert!(!match_parentheses("xx)y(aa"));
+    }
+
+    #[test]
+    fn match_parentheses_extra_opening() {
+        assert!(!match_parentheses("("));
+        assert!(!match_parentheses("x[qq]e(s"));
+        assert!(!match_parentheses("[(]"));
+        assert!(!match_parentheses("(xxú[ú]😊"));
+    }
+
+    #[test]
+    fn match_parentheses_extra_closing() {
+        assert!(!match_parentheses(")"));
+        assert!(!match_parentheses("[])"));
+        assert!(!match_parentheses("[)]"));
+        assert!(!match_parentheses("x([{)}])y"));
+    }
+
+    #[test]
+    fn match_parentheses_wrong_matched_type() {
+        assert!(!match_parentheses("[)"));
+        assert!(!match_parentheses("[qp)"));
+        assert!(!match_parentheses("[}xx"));
+        assert!(!match_parentheses("p{]vr"));
+        assert!(!match_parentheses("y[q}xx"));
+        assert!(!match_parentheses("y[qq)x"));
+        assert!(!match_parentheses("([})"));
+        assert!(!match_parentheses("(((([}))))"));
+    }
+
+    #[test]
+    fn respect_relative_ordering() {
+        assert!(!match_parentheses("([)]"));
+    }
+
+    #[test]
+    fn simple() {
+        assert!(match_parentheses("[]"));
+        assert!(match_parentheses("()"));
+        assert!(match_parentheses("{}"));
+        assert!(match_parentheses("{}{}"));
+        assert!(match_parentheses("{}{}{}"));
+        assert!(match_parentheses("{}[]()"));
+        assert!(match_parentheses("y{x}qq(xywe)[][xx]yy[][p]()"));
+    }
+
+    #[test]
+    fn nested() {
+        assert!(match_parentheses("({[]})"));
+        assert!(match_parentheses("q(x{x[xqp]yy}y)"));
+        assert!(match_parentheses("((((()))))"));
+        assert!(match_parentheses("{[({})](([]))}"));
+    }
+}
