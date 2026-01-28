4f7c154f3d5a8c57f393dccd2cf595b94c362b26
orambla64 <orambla@gmail.com>
2026-01-28 19:50:25 +0100

    03/solutions/03: oscar's solution

---
+++ 03/solutions/03-strip-prefix/src/or_strip.rs
@@ -0,0 +1,57 @@
+//! Run this file with `cargo test --test 02_strip_prefix`.
+
+// TODO: Implement a function called `strip_prefix`, which will take two strings (`needle` and `prefix`).
+// It will return a substring of `needle` starting at the first character that is not contained
+// in `prefix`.
+// See tests for examples. Take a look at the `strip_prefix_lifetime_check` test!
+//
+// Hint: you can use `string.chars()` for iterating the Unicode characters of a string.
+
+/// Below you can find a set of unit tests.
+
+pub fn strip_prefix<'a>(needle: &'a str, prefix: &str) -> &'a str {
+    let mut start_index = 0;
+    for c in needle.chars() {
+        if !prefix.contains(c) {
+            break;
+        }
+        start_index += c.len_utf8();
+    }
+    &needle[start_index..]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::strip_prefix;
+
+    #[test]
+    fn strip_prefix_basic() {
+        assert_eq!(strip_prefix("foobar", "of"), "bar");
+    }
+
+    #[test]
+    fn strip_prefix_full_result() {
+        assert_eq!(strip_prefix("foobar", "x"), "foobar");
+    }
+
+    #[test]
+    fn strip_prefix_empty_result() {
+        assert_eq!(strip_prefix("foobar", "fbaro"), "");
+    }
+
+    #[test]
+    fn strip_prefix_unicode() {
+        assert_eq!(strip_prefix("čaukymňauky", "čaukym"), "ňauky");
+    }
+
+    #[test]
+    fn strip_prefix_lifetime_check() {
+        let needle = "foobar";
+        let prefix = String::from("f");
+        let result = strip_prefix(needle, &prefix);
+        // TODO: Uncomment the `drop(prefix)` line.
+        // Does the test still work? If not, fix `strip_prefix`!
+        drop(prefix);
+        assert_eq!(result, "oobar");
+    }
+}
